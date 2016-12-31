fb4a52666ca08b9f478c3453881ab80c2ebaa830
Giorgos Angelopoulos <agelo1995@gmail.com>
2016-12-31 16:58:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/loyaltyapp/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.admin.loyaltyapp;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    private EditText user, pass;
+    private Button bLogin;
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    // JSON parser class
+
+    String username,password;
+
+    JSONParser jsonParser = new JSONParser();
+    private static final String LOGIN_URL = "https://test-cafeoino.000webhostapp.com/initC.php";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+        bLogin = (Button)findViewById(R.id.login);
+        bLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.login:
+                username = user.getText().toString();
+                password = pass.getText().toString();
+                new AttemptLogin().execute();
+                // here we have used, switch case, because on login activity you may //also want to show registration button, so if the user is new ! we can go the //registration activity , other than this we could also do this without switch //case.
+            default:
+                break;
+        }
+    }
+
+    class AttemptLogin extends AsyncTask<String, String, String> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Attempting for login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // here Check for success tag
+            int success;
+
+            try {
+
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "GET", params);
+                System.out.println(LOGIN_URL);
+                System.out.println(params);
+                // checking  log for json response
+                //Log.d("Login attempt", json.toString());
+
+                // success tag for json
+                success = json.getInt(TAG_SUCCESS);
+                System.out.println("TAG SUCCES : "+ success);
+                if (success == 1) {
+                    Log.d("Successfully Login!", json.toString());
+
+                    Intent ii = new Intent(MainActivity.this,OtherActivity.class);
+                    finish();
+                    // this finish() method is used to tell android os that we are done with current //activity now! Moving to other activity
+                    startActivity(ii);
+                    return json.getString(TAG_MESSAGE);
+                }else{
+
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        /**
+         * Once the background process is done we need to  Dismiss the progress dialog asap
+         * **/
+        protected void onPostExecute(String message) {
+
+            //pDialog.dismiss();
+            if (message != null){
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
