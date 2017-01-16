1e3c5790312c4f6afac4a5aea9d960aed73d6abd
agelogeo (PC) <agelo1995@gmail.com>
2017-01-16 18:58:01 +0200

    Create Coupon button is working now.

--- app/src/main/java/com/example/admin/loyaltyapp/ManageCouponsActivity.java
+++ app/src/main/java/com/example/admin/loyaltyapp/ManageCouponsActivity.java
@@ -21,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -60,7 +61,13 @@ public class ManageCouponsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(nameView.getText().length()==0 || requiredView.getText().length()==0){
                     Toast.makeText(ManageCouponsActivity.this,"Please fill up fields.",Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                params.clear();
+                params.add(new BasicNameValuePair("name", nameView.getText().toString()));
+                params.add(new BasicNameValuePair("required", requiredView.getText().toString()));
+                new AttemptCreateCoupon().execute();
+
             }
         });
 
@@ -146,6 +153,110 @@ public class ManageCouponsActivity extends AppCompatActivity {
             String toast_message= null;
             pDialog.dismiss();
 
+
+
+            try {
+                JSONObject output = new JSONObject(message);
+                JSONArray result = output.getJSONArray("results");
+                if (result.length() > 0) {
+                    Log.d("Get Coupons OK!", result.toString());
+
+
+                    for (int j = 0; j < result.length(); j++) {
+                        Coupon tempItem = new Coupon();
+                        JSONObject indicator = result.getJSONObject(j);
+                        tempItem.setId(indicator.getInt("id"));
+                        tempItem.setName(indicator.getString("name"));
+                        tempItem.setRequired_stamps(indicator.getInt("required_stamps"));
+                        tempItem.setOkay(true);
+                        adapterList.add(tempItem);
+                    }
+
+                    CouponAdapter myAdapter = new CouponAdapter(ManageCouponsActivity.this, adapterList);
+                    listView.setAdapter(myAdapter);
+                    toast_message="Coupons recalled successfully.";
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            JSONObject jsonResult = null;
+                            try {
+                                jsonResult = new JSONObject(message);
+                                JSONArray results = (JSONArray) jsonResult.get("results");
+                                nameView.setText( adapterList.get(position).getName() );
+                                requiredView.setText( ""+adapterList.get(position).getRequired_stamps() );
+                                EditCoupon = new Coupon(adapterList.get(position).getId(),adapterList.get(position).getName(),adapterList.get(position).getRequired_stamps());
+                                save_btn.setVisibility(View.VISIBLE);
+                                delete_btn.setVisibility(View.VISIBLE);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+                } else if(result.length()==0){
+                    toast_message = "No Coupons available.";
+
+
+                }else{
+                    toast_message =getString(R.string.barcodeExists);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (message != null){
+                Toast.makeText(ManageCouponsActivity.this, toast_message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    class AttemptCreateCoupon extends AsyncTask<String, String, String> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(ManageCouponsActivity.this);
+            pDialog.setMessage(getString(R.string.creatingCustomer));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // here Check for success tag
+
+            Log.d("request!", "starting");
+
+            JSONObject json = jsonParser.makeHttpRequest(getString(R.string.WEBSITE_URL)+getString(R.string.CREATE_COUPON_URL), "GET", params);
+            System.out.println(getString(R.string.WEBSITE_URL)+getString(R.string.CREATE_COUPON_URL));
+            System.out.println(params);
+            System.out.println(json.toString());
+            // checking  log for json response
+            //Log.d("Login attempt", json.toString());
+
+            return json.toString();
+
+
+
+        }
+        /**
+         * Once the background process is done we need to  Dismiss the progress dialog asap
+         * **/
+        protected void onPostExecute(final String message) {
+            final ArrayList<Coupon> adapterList = new ArrayList<Coupon>();
+            String toast_message= null;
+            pDialog.dismiss();
+            new AttemptGetCoupons().execute();
 
 
             try {
